60b8bfef062bf188cb2ffd742485f740b7b8041a
phx <lllphoenixlll@protonmail.com>
2020-05-03 19:08:35 -0500

    updated

--- overflow.c
+++ overflow.c
@@ -1,11 +1,14 @@
+#include <stdio.h>
+
 void return_input(void){
 
     char array[32];
     gets(array);
-    printf(“%s\n”, array);
+    printf("%s\n", array);
 }
 
 main(){
     return_input();
     return 0;
 }
+
