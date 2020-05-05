579aeee84319945f0b80a25b161ad0784da76b1c
phx <oldjamey@gmail.com>
2020-05-05 10:01:47 -0500

    updated

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ clang="$(xcrun --sdk iphoneos --find clang)"
 sdk_path="$(xcrun --sdk iphoneos --show-sdk-path)"
 
 if [[ -z $1 ]]; then
-  make -C . CC="${clang} -isysroot ${sdk_path} -arch armv7 -arch armv7s -arch arm64"
+  # make -C . CC="${clang} -isysroot ${sdk_path} -arch armv7 -arch armv7s -arch arm64"
+  make -C . CC="${clang} -isysroot ${sdk_path} -arch arm64"
 elif [[ $1 = 'llvm' ]]; then
   make llvm -C . CC="${clang} -isysroot ${sdk_path} -arch arm64"
 fi
